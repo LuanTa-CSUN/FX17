41802b94ef04d728e9dae6beedbf67e8c546ad67
Kevin Keyser <kevin.keyser@live.com>
2017-10-28 11:55:58 -0700

    Added PoseStamp Publisher and Subscriber

---
+++ Assets/PoseMsgPublisher.cs
@@ -0,0 +1,19 @@
+﻿using ROSBridgeLib.geometry_msgs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class PoseMsgPublisher
+{
+    public static string GetMessageTopic()
+    {
+        return "/mavros/setpoint_position/local";
+    }
+
+    public static string GetMessageType()
+    {
+        return PoseStampedMsg.GetMessageType();
+    }
+}
---
+++ Assets/PoseMsgSubscriber.cs
@@ -0,0 +1,33 @@
+﻿using ROSBridgeLib;
+using ROSBridgeLib.geometry_msgs;
+using SimpleJSON;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoseMsgSubscriber
+{
+    public delegate void CallBackHandler(ROSBridgeMsg msg);
+    public static event CallBackHandler OnCallBack;
+
+    public new static string GetMessageTopic()
+    {
+        return "/mavros/local_position/pose";
+    }
+
+    public new static string GetMessageType()
+    {
+        return PoseStampedMsg.GetMessageType();
+    }
+
+    public new static ROSBridgeMsg ParseMessage(JSONNode msg)
+    {
+        return new PoseStampedMsg(msg);
+    }
+
+    public new static void CallBack(ROSBridgeMsg msg)
+    {
+        OnCallBack?.Invoke(msg);
+        //Debug.Log("Render callback in /mavros/local_position/pose " + msg);
+    }
+}
