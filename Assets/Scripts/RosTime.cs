f523e3df81b98194830423c23cfcfaf92a6e21b0
Unknown <floydwin7@gmail.com>
2017-11-25 14:42:20 -0800

    added RosTime

---
+++ Assets/Scripts/RosTime.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public struct RosTime
+{
+    private static readonly DateTime unixEpoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+    public int sec;
+    public int nsec;
+
+    public static RosTime Now
+    {
+        get
+        {
+            RosTime rosTime = new RosTime();
+            TimeSpan now = DateTime.Now - unixEpoch;
+
+            rosTime.sec = (int) now.TotalSeconds;
+            rosTime.nsec = now.Milliseconds * 1000000;
+
+            return rosTime;
+        }
+    }
+}
