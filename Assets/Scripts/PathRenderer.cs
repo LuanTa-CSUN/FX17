f20dd87eb3c5ff049cee6fb5eed24e6c6191cab6
Kevin Keyser <kevin.keyser@live.com>
2017-11-25 17:51:47 -0800

    Added Route Renderering On Scene

---
+++ Assets/Scripts/PathRenderer.cs
@@ -0,0 +1,47 @@
+﻿using Sirenix.OdinInspector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(LineRenderer))]
+public class PathRenderer : SerializedMonoBehaviour
+{
+    private PathConfiguration pathConfiguration;
+
+    [ShowInInspector]
+    public PathConfiguration PathConfiguration
+    {
+        get
+        {
+            return pathConfiguration;
+        }
+        set
+        {
+            pathConfiguration = value;
+            GenerateRenderedPath();
+        }
+    }
+
+    LineRenderer lineRenderer;
+
+    public void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    private void GenerateRenderedPath()
+    {
+        List<Pose> path = pathConfiguration.GeneratePath();
+        Vector3[] positions = new Vector3[path.Count];
+        for(int i = 0; i < path.Count; i++)
+        {
+            positions[i] = path[i].Position;
+        }
+        lineRenderer.positionCount = positions.Length;
+        lineRenderer.SetPositions(positions);
+    }
+}
