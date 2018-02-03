cc16ae41fd81f184b0befeb180a5e36d4720cc19
Unknown <floydwin7@gmail.com>
2018-02-03 13:46:31 -0800

    added minimumAltitude to VehicleRenderer

--- Assets/Scripts/Vehicle/VehicleRenderer.cs
+++ Assets/Scripts/Vehicle/VehicleRenderer.cs
@@ -1,27 +1,50 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class VehicleRenderer : MonoBehaviour
 {
+	[SerializeField]
+	private float minimumAltitude;
 	
 	private VehicleController vehicleController;
 
 	public VehicleController VehicleController
 	{
 		get { return vehicleController; }
-		set { vehicleController = value; }
+		set
+		{
+			vehicleController = value;
+			Pose pose = vehicleController.PoseActual;
+			SetPose(pose.Rotation, pose.Position);
+		}
 	}
-	
-	// Update is called once per frame
-	void Update ()
+
+	private void Update ()
 	{
 		if (vehicleController == null)
 			return;
 		
 		Pose pose = vehicleController.PoseActual;
 		
-		transform.position = pose.Position;
-		transform.rotation = pose.Rotation;
+		//LerpPose(pose.Rotation, pose.Position);
+		SetPose(pose.Rotation, pose.Position);
+	}
+
+	private void LerpPose(Quaternion rotation, Vector3 worldPos)
+	{
+		worldPos = GetPositionClamped(worldPos);
+		transform.position = Vector3.Lerp(transform.position, worldPos, Time.deltaTime);
+		transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime);
+	}
+	
+	private void SetPose(Quaternion rotation, Vector3 worldPos)
+	{
+		transform.position = GetPositionClamped(worldPos);
+		transform.rotation = rotation;
+	}
+	
+	private Vector3 GetPositionClamped(Vector3 worldPos)
+	{
+		worldPos.y = Mathf.Clamp(worldPos.y, minimumAltitude, float.PositiveInfinity);
+		return worldPos;
 	}
 }
